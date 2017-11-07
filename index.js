27922fe603ccf53beeb07ca640738939547ac75d
John Burnett <tuninator@gmail.com>
2017-11-07 01:42:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -146,7 +146,7 @@ function endGame() {
   // stop checking for when arrow keys are pressed
   window.removeEventListener('keydown', moveDodger)
 
-  document.getElementByID(game).append("PLAY AGAIN?")
+  document.getElementById(game).append("PLAY AGAIN?")
 
 
 }
@@ -187,7 +187,7 @@ function moveDodgerLeft() {
        DODGER.style.left = `${dodgerLeft -= 4}px`
      }
 
-     if (dodgerLeft > 40) {
+     if (dodgerLeft > 0) {
 
 
       requestAnimationFrame(moveLeft)
